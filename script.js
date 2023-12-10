1516abc7e45f27e62629c49a7b5c325614b99867
Avinashfizz <130842159+Avinashfizz@users.noreply.github.com>
2023-12-10 17:19:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //your JS code here. If required.
 
-//let A = prompt("Enter 1st input");
+let A = prompt("Enter 1st input");
 A = 2*A;
-//let B = prompt("Enter 2nd input");
+let B = prompt("Enter 2nd input");
 let C = A+B;
 
 Console.log("The final result is: "+ C);
